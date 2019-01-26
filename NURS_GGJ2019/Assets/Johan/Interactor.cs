68c4b5c352255a9894134e2d9fc9a53847100c43
Johan Tan <johanaldrictan@gmail.com>
2019-01-26 15:43:23 -0800

    Fixed levers

---
+++ NURS_GGJ2019/Assets/Johan/Interactor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public float activationRadius;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit2D[] raycastHits = Physics2D.CircleCastAll(gameObject.transform.position, activationRadius, Vector2.zero);
+            for(int i = 0; i < raycastHits.Length; i++)
+            {
+                if(raycastHits[i].collider != null)
+                {
+                    LeverSwitch leverSwitch = raycastHits[i].collider.GetComponent<LeverSwitch>();
+                    if (leverSwitch != null)
+                    {                      
+                        leverSwitch.Activate();
+                    }
+                }
+            }
+            
+        }
+    }
+}
