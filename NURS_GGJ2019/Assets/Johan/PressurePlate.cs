3f9fa998b26f2a1a366a22843e5d7d3435cb0f63
Johan Tan <johanaldrictan@gmail.com>
2019-01-26 10:49:32 -0800

    Added Interactor and Interactable tag Added PressurePlate Started LeverSwitch

---
+++ NURS_GGJ2019/Assets/Johan/PressurePlate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+    public UnityEvent OnActivate;
+    public UnityEvent OnDeactivate;
+
+    //use this for animation
+    private bool ActivationState;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactor"))
+        {
+            ActivationState = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactor"))
+        {
+            ActivationState = false;
+        }
+    }
+}
