c54610b8bd74549ab35ee295e5a60ad851b4405a
Henry Smith <hgsmith@uci.edu>
2019-01-26 10:00:27 -0800

    Added light sensor

---
+++ NURS_GGJ2019/Assets/Henry 2.0/FollowObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public GameObject toFollow;
+
+    public void Update()
+    {
+        transform.position = new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, transform.position.z);
+    }
+}
