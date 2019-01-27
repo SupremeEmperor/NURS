a02ad3f2bb715904fb29cb42eca0643bda6047d7
Luke M <lukeam@uci.edu>
2019-01-26 21:13:06 -0800

    Scripts for level movement added

---
+++ NURS_GGJ2019/Assets/Luke/NightNextLevel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NightNextLevel : MonoBehaviour
+{
+    bool Dog = false;
+    bool Boy = false;
+
+    private void Update()
+    {
+        if(Boy == true && Dog == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Night_Boy")
+        {
+            Boy = true;
+        }
+        if (collision.gameObject.name == "Doggo")
+        {
+            Dog = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Night_Boy")
+        {
+            Boy = false;
+        }
+        if (collision.gameObject.name == "Doggo")
+        {
+            Dog = false;
+        }
+    }
+
+}
