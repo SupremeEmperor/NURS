1a6155a207c3297224c7086f46253cc8decc621c
Henry Smith <hgsmith@uci.edu>
2019-01-26 20:31:19 -0800

    Stuff

---
+++ NURS_GGJ2019/Assets/Henry 2.0/RandomInterval.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RandomInterval : MonoBehaviour
+{
+    public UnityEvent[] Activate;
+    public Vector2[] intervals;
+
+    private void Awake()
+    {
+        StartCoroutine(Flick());
+    }
+
+    private IEnumerator Flick()
+    {
+        while(true)
+        {
+            for(int i = 0; i < intervals.Length; ++i)
+            {
+                Activate[i].Invoke();
+                yield return new WaitForSeconds(Random.Range(intervals[i].x, intervals[i].y));
+            }
+        }
+    }
+}
