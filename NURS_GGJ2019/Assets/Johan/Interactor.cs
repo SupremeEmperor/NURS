6e50d4828aaadefd7ded582100b07aabad69a1ef
Henry Dan <baotd789@gmail.com>
2019-01-26 20:17:28 -0800

    jank

--- NURS_GGJ2019/Assets/Johan/Interactor.cs
+++ NURS_GGJ2019/Assets/Johan/Interactor.cs
@@ -10,14 +10,16 @@ public class Interactor : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            RaycastHit2D[] raycastHits = Physics2D.CircleCastAll(gameObject.transform.position, activationRadius, Vector2.zero);
+            RaycastHit2D[] raycastHits = Physics2D.CircleCastAll(gameObject.transform.position, activationRadius, Vector2.one);
             for(int i = 0; i < raycastHits.Length; i++)
             {
                 if(raycastHits[i].collider != null)
                 {
+                    print(raycastHits[i].collider);
                     LeverSwitch leverSwitch = raycastHits[i].collider.GetComponent<LeverSwitch>();
                     if (leverSwitch != null)
-                    {                      
+                    { 
+                        
                         leverSwitch.Activate();
                     }
                 }
