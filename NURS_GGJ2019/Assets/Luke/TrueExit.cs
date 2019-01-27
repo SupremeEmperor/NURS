4b9aff0c2a5d068de9f89177d2eaee514e5704b2
Luke M <lukeam@uci.edu>
2019-01-27 13:40:21 -0800

    LukeAll.prefab was added and preped

---
+++ NURS_GGJ2019/Assets/Luke/TrueExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrueExit : MonoBehaviour
+{
+    public Animator animator;
+
+    public void FadeToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void callAnimation()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+}
