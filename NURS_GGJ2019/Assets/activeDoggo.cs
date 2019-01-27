1b61df826ab6adc31fff1a8f7ab51ab31faa4c15
Unknown <oamarill@uci.edu>
2019-01-26 19:48:25 -0800

    Edits to Character and Night version

---
+++ NURS_GGJ2019/Assets/activeDoggo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activeDoggo : MonoBehaviour
+{
+    public bool active = false;
+    private bool activeLock = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent != null)
+        {
+            activeLock = true;
+        }
+        else
+        {
+            activeLock = false;
+        }
+        if(Input.GetButtonDown("SwitchPlayer"))
+        {
+            if(!activeLock)
+            {
+            active = !active;
+            }
+        }
+        if(active)
+        {
+            GetComponent<DoggoMovement>().enabled = true;
+        }
+        else
+        {
+            GetComponent<DoggoMovement>().enabled = false;
+        }
+    }
+}
